2013bd0241a5d2dc3954f8e3ee116d783a1de8b9
Lazar5k1 <nicholas.gill@outlook.com>
2025-01-30 01:01:06 -0500

    fix spacing/tab bug

--- raycaster-demo.cpp
+++ raycaster-demo.cpp
@@ -1,10 +1,10 @@
 //******************************************************************//
-//	Author: 		Nicholas Gill								    //
-//	Start Date:		1/30/25	    								    //
-//	End Date:		?/?/?   									    //
-//	File Name:		raycaster-demo.cpp		    				    //
-//	Version:		1.0.0										    //
-//	Description:	simple wolfenstein-esc raycaster written in     //
+//  Author:         Nicholas Gill                                   //
+//  Start Date:     1/30/25                                         //
+//  End Date:       ?/?/?                                           //
+//  File Name:      raycaster-demo.cpp                              //
+//  Version:        1.0.0                                           //
+//  Description:    simple wolfenstein-esc raycaster written in     //
 //  C++ so I can brush up my coding skills and start my game-deving //
 //  journey.                                                        //
 //******************************************************************//
