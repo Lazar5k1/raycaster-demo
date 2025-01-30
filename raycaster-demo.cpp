fc90c0820feb6adcbc713ca0a09ac307d014bf96
Lazar5k1 <nicholas.gill@outlook.com>
2025-01-30 00:54:08 -0500

    first commit

---
+++ raycaster-demo.cpp
@@ -0,0 +1,14 @@
+//******************************************************************//
+//	Author: 		Nicholas Gill								    //
+//	Start Date:		1/30/25	    								    //
+//	End Date:		?/?/?   									    //
+//	File Name:		raycaster-demo.cpp		    				    //
+//	Version:		1.0.0										    //
+//	Description:	simple wolfenstein-esc raycaster written in     //
+//  C++ so I can brush up my coding skills and start my game-deving //
+//  journey.                                                        //
+//******************************************************************//
+
+int main(){
+    return 0;
+}
